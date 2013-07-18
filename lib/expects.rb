4c49c1a67a2ad97cf0242ba65cd353a72f270303
Adam Laycock <adam@arcath.net>
2013-07-18 12:43:26 +0100

    support expects in class methods

--- lib/expects.rb
+++ lib/expects.rb
@@ -4,15 +4,29 @@ require 'expects/version'
 require 'expects/error'
 
 module Expects
-  def expects(subject, objects)
-    objects = [objects] unless objects.is_a? Array
-    raise UnexpectedInput.new(subject, objects) unless objects.include? subject.class
+  module ClassMethods
+    def expects(subject, objects)
+      objects = [objects] unless objects.is_a? Array
+      raise UnexpectedInput.new(subject, objects) unless objects.include? subject.class
+    end
+    
+    def reject(subject, objects)
+      objects = [objects] unless objects.is_a? Array
+      raise UnexpectedInput.new(subject, objects) if objects.include? subject.class
+    end
   end
   
-  def reject(subject, objects)
-    objects = [objects] unless objects.is_a? Array
-    raise UnexpectedInput.new(subject, objects) if objects.include? subject.class
+  def self.included(klass)
+    klass.extend ClassMethods
   end
   
-  private :expects
+  def expects(*args)
+    self.class.expects(*args)
+  end
+  
+  def reject(*args)
+    self.class.reject(*args)
+  end
+  
+  private :expects, :reject
 end
